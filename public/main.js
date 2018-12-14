e82ac47619569a2848b0b6c190dc9b29b1766478
Macbook Kamil <kamil@MacBook-Pro-Macbook.local>
2018-12-14 18:00:47 +0100

    Working ETL

--- public/main.js
+++ public/main.js
@@ -1,28 +1,91 @@
+var etlControl = {
+    etl: document.getElementById('etl'),
+    extract: document.getElementById('extract'),
+    transform: document.getElementById('transform'),
+    load: document.getElementById('load')
+}
 var del = document.getElementById('delete');
-var etl = document.getElementById('etl');
+var status = document.getElementById('status');
+
+
+transform.disabled = true;
+load.disabled = true;
+
+if(status === 'null'){
+    Object.keys(etlControl).forEach((i) => {
+        etlControl[i].disabled = true;
+    });
+}
 
 del.addEventListener('click', function () {
-  fetch('offers', {
-    method: 'delete'
-  })
-  .then(res => {
-    if (res.ok) return res.json()
-  })
-  .then(data => {
-    console.log(data)
-    window.location.reload()
-  })
-})
-
-etl.addEventListener('click', function () {
+    etl.disabled = false;
+    extract.disabled = false;
+    transform.disabled = true;
+    load.disabled = true;
     fetch('offers', {
-      method: 'post'
+        method: 'delete'
     })
     .then(res => {
-      if (res.ok) return res.json()
+        if (res.ok) return res.json()
     })
     .then(data => {
-      console.log(data)
-      window.location.reload()
+        console.log(data)
+        window.location.reload()
     })
-  })
+})
+
+etlControl.etl.addEventListener('click', function () {
+    Object.keys(etlControl).forEach((i) => {
+        etlControl[i].disabled = true;
+    });
+    fetch('offers', {
+        method: 'post'
+    })
+    
+    .then(data => {
+        console.log(data)
+        window.location.reload()
+    })
+})
+
+etlControl.extract.addEventListener('click', function () {
+    etlControl.etl.disabled = true;
+    etlControl.extract.disabled = true;
+    etlControl.transform.disabled = false;
+    fetch('extract', {
+        method: 'get'
+    })
+    .then(res => {
+        if (res.ok) return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        window.location.reload()
+    })
+})
+
+etlControl.transform.addEventListener('click', function () {
+    etlControl.transform.disabled = true;
+    etlControl.load.disabled = false;
+    fetch('transform', {
+        method: 'get'
+    })
+    .then(res => {
+        if (res.ok) return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        window.location.reload()
+    })
+})
+
+etlControl.load.addEventListener('click', function () {
+    etlControl.load.disabled = true;
+    fetch('load', {
+        method: 'post'
+    })
+    .then(data => {
+        console.log(data)
+        window.location.reload()
+    })
+})
--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var offers = {
     addresses: [],
     prices: []
 }
+var $;
 
 MongoClient.connect('mongodb://ETL:admin123@ds151012.mlab.com:51012/etl', (err, client) => {
     if (err) return console.log(err);
@@ -27,7 +28,10 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     db.collection('offers').find().toArray((err, result) => {
         if (err) return console.log(err)
+        JSON.stringify(result);
+        console.log(result);
         res.render('index.ejs', {offers: result})
+        console.log('GET: ' + result)
     })
 })
 
@@ -41,12 +45,17 @@ app.delete('/offers', (req, res) => {
         if(delOK) console.log('Collection deleted');
         res.redirect('/');
     })
+    offers = {
+        titles: [],
+        addresses: [],
+        prices: []
+    }
 })
 
 function scrape(res) {
     request('https://rynekpierwotny.pl/oferty/?region=11158&distance=0&type=1&rooms_0=3&rooms_1=3&price_0=400000&price_1=550000&area_0=30&area_1=70&construction_end_date=24&floor_0=5', (error, response, html) => {
         if(!error && response.statusCode == 200){
-            const $ = cheerio.load(html);
+            $ = cheerio.load(html);
             console.log('Scraping...');
             $('.offer-item-name').each((i, el) => {
                 const title = $(el).text();
@@ -62,12 +71,64 @@ function scrape(res) {
             });
 
             db.collection('offers').save(offers, (err, result) => {
-                if (err) return console.log(err)
-                console.log('saved to database')
-                res.redirect('/')
+                if (err) return console.log(err);
+                console.log('saved to database');
+                res.redirect('/');
             })
         } else {
             console.log(error);
         }
     });
+}
+
+app.get('/extract', (req, res) => {
+    extract(res);
+    console.log('Extract');
+})
+
+function extract(res) {
+    request('https://rynekpierwotny.pl/oferty/?region=11158&distance=0&type=1&rooms_0=3&rooms_1=3&price_0=400000&price_1=550000&area_0=30&area_1=70&construction_end_date=24&floor_0=5', (error, response, html) => {
+        if(!error && response.statusCode == 200){
+            $ = cheerio.load(html);
+            console.log('Scraping...');
+            return $;
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+app.get('/transform', () => {
+    transform($);
+    console.log('Transform');
+})
+
+function transform($) {
+    $('.offer-item-name').each((i, el) => {
+        const title = $(el).text();
+        offers.titles.push(title);
+    });
+    $('.offer-item-address').each((i, el) => {
+        const address = $(el).text().replace(/\s\s+/g, '');
+        offers.addresses.push(address);
+    });
+    $('.media-right strong').each((i, el) => {
+        const price = $(el).text().replace(/\s\s+/g, '');
+        offers.prices.push(price);
+    });
+    console.log('Transform');
+    console.log(offers);
+    return offers;
+}
+
+app.post('/load', (req, res) => {
+    load(res);
+})
+
+function load(res) {
+    db.collection('offers').save(offers, (err, result) => {
+        if (err) return console.log(err);
+        console.log('saved to database');
+        res.redirect('/');
+    })
 }
